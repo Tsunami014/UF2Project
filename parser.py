3f670e33f5ff7fd7ffc500cf6426850dc972e218
Tsunami014 (Max) <96847801+Tsunami014@users.noreply.github.com>
2024-09-02 07:46:11 +1000

    Added a File class and made the parser into a function

--- parser.py
+++ parser.py
@@ -1,9 +1,15 @@
-b2i = lambda b: int.from_bytes(b, byteorder='little')
+class File:
+    def __init__(self, blocks):
+        self.blocks = blocks
+    
+    def __str__(self):
+        return f'<File with {len(self.blocks)} blocks>'
+    def __repr__(self): return self.__str__()
 
 class Block:
     def __init__(self, data):
         self.flags = data[8+12]
-        self.address = int.from_bytes(data[12:16], byteorder='little')
+        self.address = data[12:16]
         self.size = int.from_bytes(data[16:20], byteorder='little') # Usually 256
         self.seq = int.from_bytes(data[20:24], byteorder='little')
         self.total = int.from_bytes(data[24:28], byteorder='little')
@@ -11,15 +17,19 @@ class Block:
         self.data = [i.strip() for i in data[32:32+self.size].hex(' ').split('00') if i.strip()]
     
     def __str__(self):
-        return f'Block: {self.seq}/{self.total} @ {self.address}: {self.data}'
+        return f'<Block {self.seq}/{self.total} @ {self.address.hex(" ")}: '+str(self.data).replace("'", "")+'>'
     def __repr__(self): return self.__str__()
 
-blocks = []
-d = 'Some data'
-with open('Sample uf2 files/arcade-test.uf2', 'rb') as f:
-    while f.readable() and d:
-        d = f.read(512)
-        if d:
-            blocks.append(Block(d))
+def parseF(file):
+    blocks = []
+    d = 'Some data'
+    with open(file, 'rb') as f:
+        while f.readable() and d:
+            d = f.read(512)
+            if d:
+                blocks.append(Block(d))
+    return File(blocks)
 
-pass
+if __name__ == '__main__':
+    f = parseF('Sample uf2 files/arcade-test.uf2')
+    pass
