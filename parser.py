e5f44d64358be4cd155e7abb64957d76d62b1c9d
Tsunami014 (Max) <Tsunami014@duck.com>
2024-09-24 21:15:34 +1000

    Added a flags class to the parser

--- parser.py
+++ parser.py
@@ -1,3 +1,20 @@
+from enum import Enum
+
+class Flags(Enum):
+    no_flags = 0
+    """Should not exist, but added for completeness"""
+    not_main_flash = 0x00000001
+    """this block should be skipped when writing the device flash; it can be used to store "comments" in the file, \
+typically embedded source code or debug info that does not fit on the device flash"""
+    file_container = 0x00001000
+    """Is a block of data which contains a file. For more info see https://github.com/microsoft/uf2?tab=readme-ov-file#file-containers"""
+    familyID_present = 0x00002000
+    """when set, the `fileSize/familyID` holds a value identifying the board family (usually corresponds to an MCU)"""
+    MD5_checksum_present = 0x00004000
+    """https://github.com/microsoft/uf2?tab=readme-ov-file#md5-checksum"""
+    extension_tags_present = 0x00008000
+    """https://github.com/microsoft/uf2?tab=readme-ov-file#extension-tags"""
+
 class File:
     def __init__(self, blocks):
         self.blocks = blocks
@@ -27,12 +44,27 @@ class File:
 
 class Block:
     def __init__(self, data):
-        self.flags = data[8+12]
+        fs = int.from_bytes(data[8:12], byteorder='little')
+        
+        #if fs == 0:
+        #    self.flags = Flags.no_flags
+        if fs == 0x00000001:
+            self.flags = Flags.not_main_flash
+        elif fs == 0x00001000:
+            self.flags = Flags.file_container
+        elif fs == 0x00002000:
+            self.flags = Flags.familyID_present
+        elif fs == 0x00004000:
+            self.flags = Flags.MD5_checksum_present
+        elif fs == 0x00008000:
+            self.flags = Flags.extension_tags_present
+        else:
+            raise ValueError(f'Unknown flag {fs}')
         self.address = data[12:16]
         self.size = int.from_bytes(data[16:20], byteorder='little') # Usually 256
         self.seq = int.from_bytes(data[20:24], byteorder='little')
         self.total = int.from_bytes(data[24:28], byteorder='little')
-        self.file_size = data[28:32]
+        self.file_size = int.from_bytes(data[28:32], byteorder='little') # File size or board family ID or zero
         self.data = [i.strip() for i in data[32:32+self.size].hex(' ').split('00') if i.strip()]
     
     def __eq__(self, other):
